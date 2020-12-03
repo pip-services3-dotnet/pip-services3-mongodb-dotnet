aa44b153ac714721e74dd74b9d849731a60fed0d
banalna <judas.priest999@gmail.com>
2020-12-03 11:43:36 +0300

    generate docs

--- docs/search/classes_0.js
+++ docs/search/classes_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['defaultmongodbfactory_59',['DefaultMongoDbFactory',['../class_pip_services3_1_1_mongo_db_1_1_build_1_1_default_mongo_db_factory.html',1,'PipServices3::MongoDb::Build']]],
-  ['dummy_60',['Dummy',['../class_pip_services3_1_1_mongo_db_1_1_persistence_1_1_dummy.html',1,'PipServices3::MongoDb::Persistence']]]
+  ['defaultmongodbfactory',['DefaultMongoDbFactory',['../class_pip_services3_1_1_mongo_db_1_1_build_1_1_default_mongo_db_factory.html',1,'PipServices3::MongoDb::Build']]]
 ];
--- docs/search/classes_1.js
+++ docs/search/classes_1.js
@@ -1,8 +1,4 @@
 var searchData=
 [
-  ['identifiablemongodbpersistence_61',['IdentifiableMongoDbPersistence',['../class_pip_services3_1_1_mongo_db_1_1_persistence_1_1_identifiable_mongo_db_persistence.html',1,'PipServices3::MongoDb::Persistence']]],
-  ['identifiablemongodbpersistence_3c_20dummy_2c_20string_20_3e_62',['IdentifiableMongoDbPersistence&lt; Dummy, string &gt;',['../class_pip_services3_1_1_mongo_db_1_1_persistence_1_1_identifiable_mongo_db_persistence.html',1,'PipServices3::MongoDb::Persistence']]],
-  ['identifiablemongodbpersistencetest_63',['IdentifiableMongoDbPersistenceTest',['../class_pip_services3_1_1_mongo_db_1_1_persistence_1_1_identifiable_mongo_db_persistence_test.html',1,'PipServices3::MongoDb::Persistence']]],
-  ['idummypersistence_64',['IDummyPersistence',['../interface_pip_services3_1_1_mongo_db_1_1_persistence_1_1_i_dummy_persistence.html',1,'PipServices3::MongoDb::Persistence']]],
-  ['innerdummy_65',['InnerDummy',['../class_pip_services3_1_1_mongo_db_1_1_persistence_1_1_inner_dummy.html',1,'PipServices3::MongoDb::Persistence']]]
+  ['identifiablemongodbpersistence',['IdentifiableMongoDbPersistence',['../class_pip_services3_1_1_mongo_db_1_1_persistence_1_1_identifiable_mongo_db_persistence.html',1,'PipServices3::MongoDb::Persistence']]]
 ];
