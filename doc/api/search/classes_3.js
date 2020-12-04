ff5e6e688a2a132b594f22c1ffe395ac7c952f27
banalna <judas.priest999@gmail.com>
2020-12-04 14:45:19 +0300

    delete component.conf.ps1 and doc folder

--- doc/api/search/classes_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['persistencefixture_73',['PersistenceFixture',['../class_pip_services3_1_1_mongo_db_1_1_persistence_1_1_persistence_fixture.html',1,'PipServices3::MongoDb::Persistence']]]
-];
--- doc/api/search/functions_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['openasync_101',['OpenAsync',['../class_pip_services3_1_1_mongo_db_1_1_persistence_1_1_mongo_db_connection.html#a74b78b89877a0c3d9f73735f1cd56219',1,'PipServices3.MongoDb.Persistence.MongoDbConnection.OpenAsync()'],['../class_pip_services3_1_1_mongo_db_1_1_persistence_1_1_mongo_db_persistence.html#a0bc2ef4b8af953d86d3ff1140b4b63e4',1,'PipServices3.MongoDb.Persistence.MongoDbPersistence.OpenAsync()'],['../class_pip_services3_1_1_mongo_db_1_1_persistence_1_1_mongo_db_persistence2.html#abfce49cf3fc36a41d861f5068ed64804',1,'PipServices3.MongoDb.Persistence.MongoDbPersistence2.OpenAsync()']]]
-];
